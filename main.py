07057ab83b72b57b7af0e281859e70f1e662766c
zazainer300 <wardian.games@gmail.com>
2025-10-16 11:30:43 +1000

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,16 @@ import time
 import threading
 from datetime import datetime, timedelta
 import pytz
+import os
 
-# Замени на свой токен и ID канала
-TOKEN = '6000570380:AAGUjahW0W9iahEKW1o7d_bo4poeswofeAc'  # Токен от BotFather
-CHANNEL_ID = -1003095096004  # ID канала (получи от @myidbot)
+# Настройки из переменных окружения
+TOKEN = os.getenv('TOKEN')
+CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
 
 bot = telebot.TeleBot(TOKEN)
 
-# Словарь для хранения времени последнего нажатия и времени отправки
 last_pill_time = {}
 
-# Список уникальных напоминаний для каждого дня
 reminders = [
     "Жопа, выпей таблетку.",
     "Жопа, время таблеток.",
@@ -25,8 +24,6 @@ reminders = [
     "Софа, выпей таблеточку."
 ]
 
-
-# Функция для отправки напоминания
 def send_reminder():
     day_of_week = datetime.now().weekday()
     message_text = reminders[day_of_week]
@@ -37,40 +34,30 @@ def send_reminder():
     sent_message = bot.send_message(CHANNEL_ID, message_text, reply_markup=keyboard)
     last_pill_time[sent_message.message_id] = {"sent_time": datetime.now(), "taken_time": None}
 
-
-# Функция проверки и отправки напоминания при просрочке
 def check_reminder():
     current_time = datetime.now()
     for message_id, times in list(last_pill_time.items()):
         sent_time = times["sent_time"]
         taken_time = times["taken_time"]
         if taken_time is None and current_time > (sent_time + timedelta(minutes=5)):
-            bot.send_message(CHANNEL_ID, "Наглая, ты не нажала кнопку! Выпей таблетку,а то по жопе получишь!")
+            bot.send_message(CHANNEL_ID, "Наглая, ты не нажала кнопку! Выпей таблетку, а то по жопе получишь!")
             last_pill_time[message_id]["taken_time"] = current_time
 
-
-# Обработчик нажатия кнопки
 @bot.callback_query_handler(func=lambda call: True)
 def handle_callback(call):
     if call.data == "took_pill":
         bot.answer_callback_query(call.id, "Отлично!")
-        praise_text = "Молодец"
+        praise_text = "Молодец!"
         bot.send_message(CHANNEL_ID, praise_text)
-        bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                      reply_markup=None)
+        bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.message_id, reply_markup=None)
         last_pill_time[call.message.message_id]["taken_time"] = datetime.now()
 
-
-# Запуск polling в отдельном потоке
 def run_bot():
     bot.polling(none_stop=True)
 
-
-# Тестовый запуск сразу
-schedule.every().day.at("11:30", tz=pytz.timezone('Asia/Vladivostok')).do(send_reminder)
+schedule.every().day.at("11:33", tz=pytz.timezone('Asia/Vladivostok')).do(send_reminder)
 schedule.every(1).minutes.do(check_reminder)
 
-# Основной цикл
 if __name__ == '__main__':
     threading.Thread(target=run_bot).start()
     while True:
